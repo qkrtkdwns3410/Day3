3ee94918bd334c49cdb97313940b7c77852bde19
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-02-26 15:37:55 +0900

    changes:

---
+++ app/src/main/java/com/example/day3/SampleProvider/MainProviderActivity.java
@@ -0,0 +1,15 @@
+package com.example.day3.SampleProvider;
+
+import com.example.day3.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainProviderActivity extends AppCompatActivity {
+	  
+	  @Override
+	  protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_main_provider);
+	  }
+}
